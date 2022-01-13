6fc78a81dd79b48329f853f68a90d5e003ed4820
fe3524 <fe212045@gmail.com>
2022-01-13 12:30:22 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :user_name,
-      :depart,
-      :position,
-      :phone
-      ])
+                                        :user_name,
+                                        :depart,
+                                        :position,
+                                        :phone
+                                      ])
   end
 end

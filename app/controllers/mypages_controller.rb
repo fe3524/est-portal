382c552b831502f7c17014ba1bb5b5097cd9cb94
fe3524 <fe212045@gmail.com>
2022-01-18 10:36:32 +0900

    prog

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,10 +1,20 @@
 class MypagesController < ApplicationController
   def index
+    @mypages = Mypage.all
   end
 
   def new
+    @mypage = Mypage.new
   end
 
   def show
   end
+
+  def create
+    Mypage.create(mypage_params)
+  end
+
+  private
+  def mypage_params
+    params.require(:mypage).permit()
 end
